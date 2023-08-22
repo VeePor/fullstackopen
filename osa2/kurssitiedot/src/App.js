d9717b8ee0d8528384949eea2b6047927ed176e6
Veeti Portimojärvi <veeti@portimojarvi.fi>
2023-08-22 22:32:01 +0300

    2.5 kurssitiedot erillinen moduuli

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-
-
+import Course from "./Course"
 
 const App = () => {
 
@@ -60,61 +58,5 @@ const App = () => {
   )
 }
 
-const Course = (props) => {
-  console.log(props.course.name)
-  return (
-    <div>
-      <Header course={props.course} />
-      <Content parts={props.course.parts} />
-      <Total parts={props.course.parts} />
-    </div>
-  )
-}
-
-const Header = (props) => {
-  return (
-    <>
-      <h2>
-        {props.course.name}
-      </h2>
-    </>
-  )
-}
-
-const Content = ({ parts }) => {
-  console.log("Content", parts)
-  let sum = 0
-  return (
-    <div>
-      {parts.map(part =>
-        <Part key={part.id} part={part} />
-      )}
-    </div>
-  )
-}
-
-const Part = ({ part }) => {
-  console.log('part test', part)
-  return (
-    <>
-      <p>
-        {part.name} {part.exercises}
-      </p>
-    </>
-  )
-}
-
-const Total = ({ parts }) => {
-  const arr = parts
-  var sum = arr.reduce((s, p) => s + p.exercises, 0)
-  console.log('sum', sum)
-  return (
-    <>
-      <b>
-        Total of {sum} exercises
-      </b>
-    </>
-  )
-}
 
 export default App
